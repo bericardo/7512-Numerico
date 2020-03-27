cc534382f5d4d137358c1c7d036555e76cc661a3
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-03-27 13:50:27 -0300

    Ejercicio 1 terminado

---
+++ Ejercicios/unidad1/ej1a.m
@@ -0,0 +1,12 @@
+# Sumatoria desde i=1 hasta N=1000 con n=i
+clear all
+close all
+
+suma = 0;
+
+for i=1:1000
+  suma= suma + i;
+endfor
+
+disp(suma)
+disp('Fin del programa')
---
+++ Ejercicios/unidad1/ej1c.m
@@ -0,0 +1,14 @@
+# Sumatoria desde i=1 hasta N=1000 con n=impar
+clear all
+close all
+
+suma = 0;
+
+for i=1:1000
+  if (rem(i,2) != 0)
+    suma= suma + i;
+  endif
+endfor
+
+disp(suma)
+disp('Fin del programa')
