6c7a42c7d14bc078702ad5fc7801758f70511dc5
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-04-14 12:29:29 -0300

    Genera los vectores a y b usando ciclos

--- Ejercicios/unidad1/ej3.m
+++ Ejercicios/unidad1/ej3.m
@@ -3,35 +3,50 @@ clear all
 
 N = 500; % Dimension del vector
 
-function escalarResultado = productoInterno(a,b)
+function resultado = crearVector(exponente,dimension)
+  nuevoVector = [];
+  
+  for i=1:dimension
+    nuevoVector(i) = i.**exponente;
+  endfor
+  
+  resultado = nuevoVector;
+endfunction
+
+function resultado = productoInterno(a,b)
   suma = 0;
   
   for i=1:rows(a)
     suma = suma + a(i,1)*(b(i,1));
   endfor
   
-  escalarResultado = suma;
+  resultado = suma;
 endfunction
 
-function distanciaResultado = distancia(a,b)
+function resultado = distancia(a,b)
   suma = 0;
   
   for i=1:rows(a)
     suma = suma + ((b(i,1)-a(i,1))^2);
   endfor
   
-  distanciaResultado = sqrt(suma);
+  resultado = sqrt(suma);
 endfunction
 
 % Creo vectores de Nx1 con valores aleatorios
-a = rand(N,1);
-b = rand(N,1);
+a = crearVector(1,N);
+b = crearVector(2,N);
 
-b = b.**2; % Eleva todos los elementos del vector b al cuadrado
+disp('Producto Interno:');
+disp(productoInterno(a,b));
 
-disp(productoInterno(a,b))
+disp('Norma cuadrado:');
 disp(norm(b-a,2))
+
+disp('Norma infinito:');
 disp(norm(b-a,inf))
+
+disp('Distancia entre vectores:');
 disp(distancia(a,b))
 
-disp('Fin del programa')
+disp('Fin del programa');
