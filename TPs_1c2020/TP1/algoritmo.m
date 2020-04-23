026dd1f8184e7404d92b59b9814e578490cdb711
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-04-23 17:54:07 -0300

    Comenta lo que falta revisar

--- TPs_1c2020/TP1/algoritmo.m
+++ TPs_1c2020/TP1/algoritmo.m
@@ -6,6 +6,38 @@ datos = load('Corrientes_1960-2020.dat');
 
 %-------------------------------FUNCIONES--------------------------------------%
 
+% vector1: es un vector de una columna
+% vector2: es un vector de una columna
+% y_label: es la etiqueta que corresponde al eje Y al plotear
+% x_label: es la etiqueta que corresponde al eje X al plotear
+% sonFechas: es un booleano que seteamos a true si vector1 es un vector de fechas
+%
+% Hace un grafico. En este caso, vector2 siempre tiene los niveles hidrometricos
+function resultado = graficar(vectorFechas, vectorNivelesHidrometricos, titulo)
+  % Lista de elementos a graficar
+  legend_list = {"Nivel Hidrometrico"};
+  
+  % Graficador
+  plot(vectorFechas, vectorNivelesHidrometricos, '-');
+  
+  datetick(gca);
+  
+  legend(legend_list);
+
+  % Título
+  title(titulo);
+
+  % Rótulos de ejes
+  xlabel("Fecha",'fontsize',10)
+  ylabel("Nivel Hidrometrico",'fontsize',14)
+  
+  % Grilla
+  grid
+
+  % Tamaño de letra de los n
+  set(gca,'fontsize',20); % sets font of numbers on axes 
+endfunction
+
 % datos: matriz con los datos tal cual los cargamos desde el inicio del programa
 %
 % Devuelve un vector con las fechas transformadas de manera que pueda ser ploteada
@@ -43,50 +75,15 @@ function resultado = minimos_anuales(datos)
       if(datos(i,4) < minimo_anual)
         dia = datos(i,1);
         mes = datos(i,2);
-        
         minimo_anual = datos(i,4);
       endif
       i = i + 1;
     endwhile
     
     matrizMinimosAnuales(j,:) = [dia, mes, anio, minimo_anual];
-
     j = j + 1;
   endwhile
   resultado = matrizMinimosAnuales;
-endfunction
-
-% vector1: es un vector de una columna
-% vector2: es un vector de una columna
-% y_label: es la etiqueta que corresponde al eje Y al plotear
-% x_label: es la etiqueta que corresponde al eje X al plotear
-% sonFechas: es un booleano que seteamos a true si vector1 es un vector de fechas
-%
-% Hace un grafico. En este caso, vector2 siempre tiene los niveles hidrometricos
-function resultado = graficar(vectorFechas, vectorNivelesHidrometricos, titulo)
-  
-  % Lista de elementos a graficar
-  legend_list = {"Nivel Hidrometrico"};
-  
-  % Graficador
-  plot(vectorFechas, vectorNivelesHidrometricos, '-');
-  
-  datetick(gca);
-  
-  legend(legend_list);
-
-  % Título
-  title(titulo);
-
-  % Rótulos de ejes
-  xlabel("Fecha",'fontsize',10)
-  ylabel("Nivel Hidrometrico",'fontsize',14)
-  
-  % Grilla
-  grid
-
-  % Tamaño de letra de los n
-  set(gca,'fontsize',20); % sets font of numbers on axes 
 endfunction
 
 function resultado = minimos_mensuales(datos)
@@ -119,49 +116,49 @@ function resultado = minimos_mensuales(datos)
 endfunction
 
 # Devuelve el primer nivel hidrometrico del mes pasado por parametro que aparece
-function resultado = obtener_un_minimo_del_mes(datos,mes)
-  vector = [];
+%function resultado = obtener_un_minimo_del_mes(datos,mes)
+%  vector = [];
   
-  for i=1:rows(datos)
-    if datos(i,2) == mes
-      vector = datos(i,:);
-      break
-    endif
-  endfor
+%  for i=1:rows(datos)
+%    if datos(i,2) == mes
+%      vector = datos(i,:);
+%      break
+%    endif
+%  endfor
   
-  resultado = vector;
-endfunction
+%  resultado = vector;
+%endfunction
 
-function resultado = min_mes(datos)
-  matrizMinimosMensuales = [];
+%function resultado = min_mes(datos)
+%  matrizMinimosMensuales = [];
   
-  for mes=1:12
-    vectorMinimo = obtener_un_minimo_del_mes(datos,mes); # obtiene primer nivel del mes que aparece
-    for i=1:rows(datos)
-      if datos(i,2) == mes && datos(i,4) < vectorMinimo(4);
-        vectorMinimo = datos(i,:);
-      endif
-    endfor
+%  for mes=1:12
+%    vectorMinimo = obtener_un_minimo_del_mes(datos,mes); # obtiene primer nivel del mes que aparece
+%    for i=1:rows(datos)
+%      if datos(i,2) == mes && datos(i,4) < vectorMinimo(4);
+%        vectorMinimo = datos(i,:);
+%      endif
+%    endfor
     
-    matrizMinimosMensuales(mes,:) = vectorMinimo;
-  endfor
-  resultado = matrizMinimosMensuales;
-endfunction
+%    matrizMinimosMensuales(mes,:) = vectorMinimo;
+%  endfor
+%  resultado = matrizMinimosMensuales;
+%endfunction
 
 % Devuelve la posicion del minimo absoluto
-function resultado = minimo_absoluto(datos)
-  min = datos(1,4);
-  pos = 1;
+%function resultado = minimo_absoluto(datos)
+%  min = datos(1,4);
+%  pos = 1;
+%  
+%  for i=1:rows(datos)
+%    if datos(i,4) < min
+%      min = datos(i,4);
+%      pos = i;
+%    endif
+%  endfor
   
-  for i=1:rows(datos)
-    if datos(i,4) < min
-      min = datos(i,4);
-      pos = i;
-    endif
-  endfor
-  
-  resultado = pos;
-endfunction
+%  resultado = pos;
+%endfunction
 
 % Devuelve matriz con el rango del periodo especificado
 function resultado = recortar_periodo(datos,desdeAnio,hastaAnio)
@@ -199,13 +196,13 @@ endfunction
 %-------------------------------FIN DE FUNCIONES-------------------------------%
 
 % Punto a del ejercicio
-%vectorFechas = convertirFechas(datos);
-%graficar(vectorFechas, datos(:,4), "Serie Completa - FIUBA - 75.12");
+vectorFechas = convertirFechas(datos);
+graficar(vectorFechas, datos(:,4), "Serie Completa - FIUBA - 75.12");
 
 % Punto b del ejercicio
-%matrizMinimosAnuales = minimos_anuales(datos);
-%vectorFechas = convertirFechas(matrizMinimosAnuales);
-%graficar(vectorFechas,matrizMinimosAnuales(:,4),"Serie de Minimos Anuales - FIUBA - 75.12");
+matrizMinimosAnuales = minimos_anuales(datos);
+vectorFechas = convertirFechas(matrizMinimosAnuales);
+graficar(vectorFechas,matrizMinimosAnuales(:,4),"Serie de Minimos Anuales - FIUBA - 75.12");
 
 % Punto c del ejercicio
 %matrizMinimosMensuales = minimos_mensuales(datos);
@@ -216,7 +213,7 @@ endfunction
 matrizMinimosAnuales = minimos_anuales(datos);
 nuevoPeriodo = recortar_periodo(matrizMinimosAnuales, 1975,2020);
 
-% No se pide graficar, asi que esto no es necesario hacerlo.
+% No se pide graficar en el punto d, asi que esto no es necesario hacerlo.
 %vectorFechas = convertirFechas(nuevoPeriodo);
 %graficar(vectorFechas,nuevoPeriodo(:,4),"Serie 1975 - 2020 - FIUBA - 75.12");
 
