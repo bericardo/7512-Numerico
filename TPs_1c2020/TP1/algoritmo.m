0c5d22607a6b18115add052c6295aec4bf5292b1
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-05-03 11:03:39 -0300

    Cambia grafico serie minimos mensuales

--- TPs_1c2020/TP1/algoritmo.m
+++ TPs_1c2020/TP1/algoritmo.m
@@ -35,7 +35,7 @@ function resultado = graficar(vectorFechas, vectorNivelesHidrometricos, titulo)
   % Tamaño de letra de los n
   set(gca,'fontsize',20); % sets font of numbers on axes
   
-  %print -djpg serie_mes_promedio_minimo_1969.jpg
+  print -djpg serie_minimos_mensuales.jpg
 endfunction
 
 % datos: matriz con los datos tal cual los cargamos desde el inicio del programa
@@ -181,13 +181,13 @@ endfunction
 %-------------------------------FIN DE FUNCIONES-------------------------------%
 
 % Punto a del ejercicio
-vectorFechas = convertirFechas(datos);
-graficar(vectorFechas, datos(:,4), "Serie Completa - FIUBA - 75.12");
+%vectorFechas = convertirFechas(datos);
+%graficar(vectorFechas, datos(:,4), "Serie Completa - FIUBA - 75.12");
 
 % Punto b del ejercicio
-matrizMinimosAnuales = minimos_anuales(datos);
-vectorFechas = convertirFechas(matrizMinimosAnuales);
-graficar(vectorFechas,matrizMinimosAnuales(:,4),"Serie de Minimos Anuales - FIUBA - 75.12");
+%matrizMinimosAnuales = minimos_anuales(datos);
+%vectorFechas = convertirFechas(matrizMinimosAnuales);
+%graficar(vectorFechas,matrizMinimosAnuales(:,4),"Serie de Minimos Anuales - FIUBA - 75.12");
 
 % Punto c del ejercicio
 matrizPromediosMensuales = minimos_mensuales(datos);
@@ -197,15 +197,15 @@ vectorMesMinimo = obtener_mes_minimo_promedio(matrizPromediosMensuales);
 
 %periodoRecortado = recortar_periodo(datos,1969,1969); 
 vectorFechas = convertirFechas(matrizPromediosMensuales);
-graficar(vectorFechas,matrizPromediosMensuales(:,4),"Serie Mes Promedio Minimo Ano 1969 - FIUBA - 75.12");
+graficar(vectorFechas,matrizPromediosMensuales(:,4),"Serie Minimos Mensuales - FIUBA - 75.12");
 
 % Punto d del ejercicio
-matrizMinimosAnuales = minimos_anuales(datos);
-nuevoPeriodo = recortar_periodo(matrizMinimosAnuales, 1975,2020);
+%matrizMinimosAnuales = minimos_anuales(datos);
+%nuevoPeriodo = recortar_periodo(matrizMinimosAnuales, 1975,2020);
 
 % No se pide graficar en el punto d, asi que esto no es necesario hacerlo.
 %vectorFechas = convertirFechas(nuevoPeriodo);
 %graficar(vectorFechas,nuevoPeriodo(:,4),"Serie 1975 - 2020 - FIUBA - 75.12");
 
-periodoOrdenado = bubblesort(nuevoPeriodo); # Ordena de menor a mayor por niveles hidrometricos
-dlmwrite('ranking_5_principales_bajantes.csv', periodoOrdenado); #Exportar datos como archivo .csv
+%periodoOrdenado = bubblesort(nuevoPeriodo); # Ordena de menor a mayor por niveles hidrometricos
+%dlmwrite('ranking_5_principales_bajantes.csv', periodoOrdenado); #Exportar datos como archivo .csv
