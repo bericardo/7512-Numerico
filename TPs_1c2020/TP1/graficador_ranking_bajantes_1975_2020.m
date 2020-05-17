95c5456f01bf48fc4ae13ecaba6f1192430910cb
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-05-17 13:22:49 -0300

    TP1: Agrega 5 dias mas en el eje x

--- TPs_1c2020/TP1/graficador_ranking_bajantes_1975_2020.m
+++ TPs_1c2020/TP1/graficador_ranking_bajantes_1975_2020.m
@@ -24,20 +24,20 @@ hold off
 
 % Titulo y legendas
 title("Ranking 5 1975-2020 - FIUBA - 75.12");
-legend("Marzo 2020","Agosto 2001","Septiembre 2001","Agosto 2009","Enero 1989");
+legend("Marzo 2020","Agosto 2001","Agosto 2009","Enero 1989","Enero 1986");
 
 % Configuracion de absisas 
 xlabel("Dia",'fontsize',10);
 ylabel("Nivel Hidrometrico",'fontsize',14);
 set(gca,'fontsize',20);
-set(gca (), "xlim",[0,31]);
+set(gca (), "xlim",[0,35]);
 
 % Agrega texto incrustado
-text(31,1.28,"1.31");
-text(31,1.4,"1.4");
-text(31,2,"1.4");
-text(31,3,"1.47");
+text(31.3,1.28,"1.31");
+text(31.3,1.4,"1.4");
+text(31,3.1,"1.47");
 text(31,5,"1.54");
+text(31,2.6,"1.6");
 
 % Exportamos el grafico
-print -djpg "ranking_bajantes_1975-2020.jpg"
+print -djpg "ranking_bajantes_1975-2020"
