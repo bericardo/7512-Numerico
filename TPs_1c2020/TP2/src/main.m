8e261a97d1e072b6de24ca76089a7635da6c8f15
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-06-04 10:00:23 -0300

    Ahora la resta se hace cuando se calcula el valor de la incognita al realizar la sustitucion inversa

--- TPs_1c2020/TP2/src/main.m
+++ TPs_1c2020/TP2/src/main.m
@@ -116,13 +116,13 @@ function vector_soluciones = sustitucion_inversa(A_ampliada)
     % Reemplaza las incognitas conocidas y los paso al segundo miembro
     for j=i+1:columns(A_ampliada)-1
       if (j != i)
-        suma = suma + A_ampliada(i,j)*(-1)*soluciones(j);
+        suma = suma + A_ampliada(i,j)*soluciones(j);
       endif
     endfor
     
     % Calcula el valor de la incognita y se guarda en el vector soluciones
     b = A_ampliada(i,columns(A_ampliada));
-    soluciones(i) = (b + suma) / A_ampliada(i,i);
+    soluciones(i) = (b - suma) / A_ampliada(i,i);
   endfor
   vector_soluciones = soluciones;
 endfunction
