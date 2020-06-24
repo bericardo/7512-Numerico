fa8b4b08f0e68212a544ae4daf85c090e9e09a35
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2020-06-24 08:03:42 -0300

    Borra dos parámetros  de la función biseccion
    Los valores de estas variables se pisan al entrar a la función, no tiene sentido que la función reciba estas variables.

--- Scripts/bisection.m
+++ Scripts/bisection.m
@@ -6,7 +6,7 @@ function y = f(x)
   y = ((x.^2) / 4) - sin(x);
 endfunction
 
-function nro_iteraciones = biseccion_t(a,b,tol,f_a=0,f_b=0,nro_iter=0)
+function nro_iteraciones = biseccion_t(a,b,tol,nro_iter=0)
   m = (a+b)/2;
   err_abs = abs((b-a)/2);
   f_a = f(a);
@@ -15,15 +15,15 @@ function nro_iteraciones = biseccion_t(a,b,tol,f_a=0,f_b=0,nro_iter=0)
   if (err_abs > tol)
     nro_iter += 1;
     if (f_a*f_b < 0)
-      nro_iter = biseccion_t(a,m,tol,f_a,f_b,nro_iter);
+      nro_iter = biseccion_t(a,m,tol,nro_iter);
     else
-      nro_iter = biseccion_t(m,b,tol,f_a,f_b,nro_iter);
+      nro_iter = biseccion_t(m,b,tol,nro_iter);
     endif
   endif
   nro_iteraciones = nro_iter;
 endfunction
 
-function error_absoluto = biseccion_c(a,b,cant_iter,f_a=0,f_b=0,nro_iter=0)
+function error_absoluto = biseccion_c(a,b,cant_iter,nro_iter=0)
   m = (a+b)/2;
   err_abs = abs((b-a)/2);
   f_a = f(a);
@@ -32,9 +32,9 @@ function error_absoluto = biseccion_c(a,b,cant_iter,f_a=0,f_b=0,nro_iter=0)
   if (cant_iter ~= nro_iter)
     nro_iter += 1;
     if (f_a*f_b < 0)
-      err_abs = biseccion_c(a,m,cant_iter,f_a,f_b,nro_iter);
+      err_abs = biseccion_c(a,m,cant_iter,nro_iter);
     else
-      err_abs = biseccion_c(m,b,cant_iter,f_a,f_b,nro_iter);
+      err_abs = biseccion_c(m,b,cant_iter,nro_iter);
     endif
   endif
   error_absoluto = err_abs;
