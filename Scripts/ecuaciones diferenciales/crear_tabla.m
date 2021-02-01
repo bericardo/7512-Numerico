d07789ea1e2dd21b53e27c9e54c80a0575e2fb5e
Ricardo Brandan <brandanricardo@yahoo.com.ar>
2021-02-01 11:18:49 -0300

    Elimina comentarios

--- Scripts/ecuaciones diferenciales/crear_tabla.m
+++ Scripts/ecuaciones diferenciales/crear_tabla.m
@@ -1,16 +1,14 @@
-% Crea tabla de datos que se usa con el metodo del trapecio y simpson
-% f es la funcion que se quiere integrar
-% lim_inf es el limite de integracion inferior
-% lim_sup es el limite de integracion superior
 function tabla_resultado = crear_tabla(f,lim_inf,lim_sup,h)
   tabla = [lim_inf; f(lim_inf)];
   x_sig = lim_inf + h;
   columna = 2;
+  
   while (x_sig < lim_sup)
     tabla(:,columna) = [x_sig;f(x_sig)];
     x_sig += h;
     columna += 1;
   endwhile
+  
   tabla(:,columna) = [lim_sup;f(lim_sup)];
   tabla_resultado = tabla;
 endfunction
